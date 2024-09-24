4761b41b05aaab8acc706910ca21457b03bcc472
d3or <deorrunner@gmail.com>
2024-09-24 17:21:21 -0400

    chore: update semantic release branch

--- publishing/release-rules.cjs
+++ publishing/release-rules.cjs
@@ -1,24 +1,4 @@
-module.exports = {
-  branches: ['main', 'beta', 'abstract'],
-  plugins: [
-    ['@semantic-release/commit-analyzer', {
-      preset: 'angular',
-      releaseRules: [
-        { type: 'feat', scope: 'breaking', release: 'major' },
-        { scope: 'public', release: 'patch' },
-        { type: 'feat', release: 'minor' },
-        { type: 'fix', release: 'patch' },
-        { type: 'docs', release: 'patch' },
-        { type: 'style', release: 'patch' },
-        { type: 'refactor', release: 'patch' },
-        { type: 'perf', release: 'patch' },
-        { type: 'test', release: 'patch' },
-        { type: 'ci', release: 'patch' },
-        { type: 'chore', release: 'patch' },
-      ],
-    }],
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/npm',
-    '@semantic-release/github',
-  ],
-};
+module.exports = [
+  { type: "feat", scope: "breaking", release: "major" },
+  { scope: "public", release: "patch" },
+];
